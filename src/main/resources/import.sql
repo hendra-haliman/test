eb0a8a0f09cf5742965e4114d6b0c452379d3541
hendr <hendr@DESKTOP-FENRDMT>
2023-11-16 16:42:41 +0700

    update sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,17 @@
 insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-01", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-02", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-02", "Yos Sudarso", "Jl Panjang 29", "0878822774", "yos.sudarso@gmail.com", "WNI", "L", 23);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-03", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-03", "Dewi S.", "Jl Cideng no 45", "0808121212", "dewi.s@gmail.com", "WNI", "P", 17);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-04", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-04", "John Doe", "Jl Tentara Pelajar no 1", "0251-09080706", "john@outlook.com", "WNI", "P", 45);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-05", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-05", "Haryanto", "Jl Panjang 129", "08912444555", "haryanto@gmail.com", "WNA", "L", 33);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-06", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-06", "Susi Susanti", "Jl Tanah Abang II no 23", "08127712000", "susi@biznet.co.id", "WNI", "P", 15);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-07", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-07", "Jalak Harupat", "Bogor Timur, Kota Bogor", "0251-33322289", "jalak@gmail.com", "WNI", "L", 29);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-08", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-08", "Srikandi", "Jl Lebak no 3", "08111100023", "srikandi@aol.com", "WNA", "L", 51);
 
-insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-09", "Ahmad Yani", "Jl Panjang 29", "08123456789", "ahmad.yani@gmail.com", "WNI", "L", 20);    
+insert into user(customer_id, customer_name, alamat, nomor_hp, email, kewarganegaraan, gender, usia) values("CST-09", "Joel Petersen", "Jl Suniaraja 28, Bandung", "022128735", "joel@gmail.com", "WNA", "L", 22);    
